0b6c5bc8070bde2038c0e6e8f642962e9702d269
NavaneethaBonala <navaneethabonala@gmail.com>
2025-04-09 23:17:53 +0530

    Added few changes to Java demo file

--- src/main/java/selenuimBasics/JavaDemo.java
+++ src/main/java/selenuimBasics/JavaDemo.java
@@ -5,6 +5,8 @@ public class JavaDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Selenium");
+		System.out.println("/ثشقىهىل ");
+		System.out.println("Learning Selenium with JAVA");
 
 	}
 
