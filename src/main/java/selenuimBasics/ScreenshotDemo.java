7e77ddf8ce0ab71e3382ff2406cc011107766bb3
NavaneethaBonala <navaneethabonala@gmail.com>
2025-04-25 23:37:31 +0530

    adding the screenshots feature in selenium
    in this file it is discussed that how to handle the screenshots when a test case is failed

---
+++ src/main/java/selenuimBasics/ScreenshotDemo.java
@@ -0,0 +1,30 @@
+package selenuimBasics;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.io.Files;
+
+public class ScreenshotDemo {
+
+	public static void main(String[] args) throws IOException {
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/");
+		// to save the screenshots , create a new folder in root with name screenshots,
+		// and take path of it
+		String path = "./ScreenShots";
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File temp = ts.getScreenshotAs(OutputType.FILE);
+
+		Files.copy(temp, new File(path));
+	}
+
+}
