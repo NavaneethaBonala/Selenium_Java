6889fcc1f58718ea7c8e949fdf2f578c745feeeb
NavaneethaBonala <navaneethabonala@gmail.com>
2025-04-26 21:06:42 +0530

    Update ScreenshotDemo.java

--- src/main/java/selenuimBasics/ScreenshotDemo.java
+++ src/main/java/selenuimBasics/ScreenshotDemo.java
@@ -12,19 +12,26 @@ import com.google.common.io.Files;
 
 public class ScreenshotDemo {
 
-	public static void main(String[] args) throws IOException {
+	public static void captureScreenshot(String tcname) throws IOException {
 
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://www.facebook.com/");
 		// to save the screenshots , create a new folder in root with name screenshots,
 		// and take path of it
-		String path = "./ScreenShots/abc.png";
+		String path = "./ScreenShots/" + tcname + ".png";
 
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File temp = ts.getScreenshotAs(OutputType.FILE);
 
 		Files.copy(temp, new File(path));
+
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		captureScreenshot("Login Page");
+
 	}
 
 }
