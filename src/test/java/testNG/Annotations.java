d08127a88de5e0692332ae9774f39d43d746afff
NavaneethaBonala <navaneethabonala@gmail.com>
2025-05-02 20:45:40 +0530

    updating the TestNG files
    added, before and after conditions

--- src/test/java/testNG/Annotations.java
+++ src/test/java/testNG/Annotations.java
@@ -1,12 +1,46 @@
 package testNG;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Annotations {
 	
+	@BeforeSuite
+	public  void dbconnection()
+	{
+		System.out.println("Connecting to DB");
+	}
+	
+	@AfterSuite
+	public  void sendReport()
+	{
+		System.out.println("Sending Status Report");
+	}
+	
+	@BeforeMethod
+	public  void browserSetup()
+	{
+		System.out.println("Setting Up Browser");
+	}
+	
 	@Test
 	public  void tc1()
 	{
-		System.out.println("This is TC 1");
+		System.out.println("FB login ");
+	}
+	
+	
+	@Test
+	public  void SignUp()
+	{
+		System.out.println("FB SignUp");
+	}
+	
+	@Test
+	public  void closeBrowser()
+	{
+		System.out.println("Closing browser");
 	}
 }
